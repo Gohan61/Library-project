142bed88840b0df0b6c02b0512bcd25684836f3b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-01-30 16:12:18 +0100

    Add remove button to delete book from library

--- play.js
+++ play.js
@@ -14,17 +14,34 @@ function Book(author, title, pages, read) {
 
 function displayBooks(array) {
   const newDiv = document.createElement("div");
+  const removeButton = document.createElement("button");
   const displayAuthor = document.createElement("p");
   const displayTitle = document.createElement("p");
   const displayPages = document.createElement("p");
   const displayRead = document.createElement("p");
+  const indexInLibrary = myLibrary
+    .map((e) => e.author)
+    .indexOf(array.slice(-1)[0]["author"]);
 
+  newDiv.setAttribute("index", indexInLibrary);
+
+  removeButton.textContent = "Remove book from library";
+  removeButton.addEventListener("click", () => {
+    newDiv.remove();
+  });
   displayAuthor.textContent = "Author: " + array.slice(-1)[0]["author"];
   displayTitle.textContent = "Title: " + array.slice(-1)[0]["title"];
   displayPages.textContent = "Pages: " + array.slice(-1)[0]["pages"];
   displayRead.textContent = "Read: " + array.slice(-1)[0]["read"];
+
   library.appendChild(newDiv);
-  newDiv.append(displayAuthor, displayTitle, displayPages, displayRead);
+  newDiv.append(
+    displayAuthor,
+    displayTitle,
+    displayPages,
+    displayRead,
+    removeButton
+  );
 }
 
 addNewBook.addEventListener("click", () => {
